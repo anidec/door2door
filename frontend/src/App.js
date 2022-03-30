ca9aa613d55b23353e3ca4910a61bf6cf832ecfb
Animesh Kaushik <animeshkaushik10@gmail.com>
2022-03-30 18:44:59 +0530

    router set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signin" element={<Register />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,42 +1,64 @@
-import React from 'react'
-import '../styles/navbar.css';
-import mainLogo from '../images/logo.png';
+import React from "react";
+import "../styles/navbar.css";
+import mainLogo from "../images/logo.png";
+import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light navsize shadow p-3 mb-5 bg-white rounded">
-      <div className='container-fluid nc'>
-        <a className="navbar-brand" href="#"><img className='image1' src={mainLogo} /></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Book</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Join</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Help</a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Log In</a>
-            </li>
-            <button type="button" className="btn getstarted btn-primary">Get Started</button>
-          </ul>
-        </div>
+        <div className="container-fluid nc">
+          <a className="navbar-brand" href="#">
+            <img className="image1" src={mainLogo} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/book">
+                  Book
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/join">
+                  Join
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signin">
+                  Help
+                </NavLink>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Log In
+                </NavLink>
+              </li>
+              <button type="button" className="btn getstarted btn-primary">
+                Get Started
+              </button>
+            </ul>
+          </div>
         </div>
       </nav>
-
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
